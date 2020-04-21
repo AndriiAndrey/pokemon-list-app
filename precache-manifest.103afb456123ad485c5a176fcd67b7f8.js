45398109fa98a81ca3812e2b87deee4a1ff06af2
AndriiAndrey <semykinandrey@gmail.com>
2020-04-21 18:53:53 +0300

    Updates

---
+++ precache-manifest.103afb456123ad485c5a176fcd67b7f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcad02a3d863b28ae025741f8d761ff1",
+    "url": "/pokemon-list-app/index.html"
+  },
+  {
+    "revision": "a0e61974f435f75c90aa",
+    "url": "/pokemon-list-app/static/css/main.6cf33dad.chunk.css"
+  },
+  {
+    "revision": "45502945d04aab02d313",
+    "url": "/pokemon-list-app/static/js/2.0f8bb006.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/pokemon-list-app/static/js/2.0f8bb006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e61974f435f75c90aa",
+    "url": "/pokemon-list-app/static/js/main.4c84f423.chunk.js"
+  },
+  {
+    "revision": "37334c16c39a917183c9",
+    "url": "/pokemon-list-app/static/js/runtime-main.47688c15.js"
+  }
+]);
